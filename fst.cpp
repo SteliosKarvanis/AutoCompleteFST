42ab0925896910ecde5f0a68701a6a4da61846b2
stelios <stelioscbranco@hotmail.com>
2023-10-15 13:03:47 -0300

    adding comments and improving code style

--- fst.cpp
+++ fst.cpp
@@ -68,28 +68,35 @@ Node* FST::retrieve_node_with_prefix(const std::string& prefix){
 /////////////////////////////// BUILD UTILS ////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////
 
+/// @brief Build the FST from the words in the given file
+/// @param filename: the file with the words (must be ordered, to check it, use `check_data()`)
 void FST::buildFST(const std::string& filename){
-    std::ifstream myfile;
     std::string word;
-    int common_prefix_size;
+    int existent_prefix_size;
     Node* last_common_node;
-    myfile.open(filename);
+    std::ifstream myfile(filename);
     while(std::getline(myfile, word)){
-        last_common_node = get_new_word_max_common_preffix(word, common_prefix_size);
+        last_common_node = get_new_word_max_existent_prefix(word, existent_prefix_size);
         this->update(last_common_node);
-        this->froze_node_tree(get_next_node_with_last_char(last_common_node));
-        this->add_suffix(last_common_node, word, common_prefix_size);
+        this->froze_node_tree(get_last_next_node(last_common_node));
+        this->add_suffix(last_common_node, word, existent_prefix_size);
     }
     this->update(last_common_node);
 }
 
+/// @brief Froze all the node tree from the given node (including the base node)
+/// @param node: the base node to froze the tree from
 void FST::froze_node_tree(Node* node){
+    // if node is already frozen, all her tree should be frozen
     if(node == nullptr || node->frozen)
         return;
 
     node->frozen = true;
+    // Update the final frozen node
     if(node->valid && node->next_nodes.empty() && this->final_frozen_node == nullptr)
         this->final_frozen_node = node;
+    
+    // Recursively froze the tree
     for(auto next_node : node->next_nodes){
         if(next_node->frozen)
             continue;
@@ -97,16 +104,21 @@ void FST::froze_node_tree(Node* node){
     }
 }
 
-Node* FST::get_next_node_with_last_char(Node* node){
+/// @brief Get the next node added to the given node (usefull in FST build)
+Node* FST::get_last_next_node(Node* node){
     if(node->next_nodes.empty())
         return nullptr;
     return node->next_nodes[node->next_nodes.size() - 1];
 }
 
-char FST::get_next_last_char(Node* node){
+/// @brief Get the char related to the last transition from the given node (usefull in FST build)
+char FST::get_last_next_char(Node* node){
     return node->forward_transitions[node->forward_transitions.size() - 1]->character;
 }
 
+/// @brief Add a new node to the fst, building all the transaction related
+/// @param base_node: the parent node to the new node
+/// @param transition: the transition to the new node
 void FST::add_node(Node* base_node, Transition* transition){
     Node* new_node = new Node();
 
@@ -117,83 +129,99 @@ void FST::add_node(Node* base_node, Transition* transition){
     new_node->backward_transitions.push_back(transition);
 }
 
-Node* FST::get_new_word_max_common_preffix(const std::string& new_word, int& common_prefix_size){
+/// @brief Get the node with the max common prefix with the new word
+/// @param new_word: the word that is been added
+/// @param existent_prefix_size: output, the size of the common prefix
+/// @return the node with the max common prefix with the new word
+Node* FST::get_new_word_max_existent_prefix(const std::string& new_word, int& existent_prefix_size){
     Node* actual_node = this->root;
-    common_prefix_size = 0;
+    existent_prefix_size = 0;
     for(char current_char : new_word){
-        if(actual_node->next_nodes.empty() || get_next_last_char(actual_node) != current_char)
+        if(actual_node->next_nodes.empty() || get_last_next_char(actual_node) != current_char)
             break;
-        common_prefix_size++;
-        actual_node = get_next_node_with_last_char(actual_node);
+        existent_prefix_size++;
+        actual_node = get_last_next_node(actual_node);
     }
     return actual_node;
 }
 
-void FST::add_suffix(Node* base_node, const std::string& word, int initial_depth){
+/// @brief Add the suffix of the new word to the fst
+/// @param base_node: should be the node with the biggest common prefix with the new word
+/// @param word: the word that is been added
+/// @param common_prefix_size: the size of the biggest common prefix
+void FST::add_suffix(Node* base_node, const std::string& word, int common_prefix_size){
     Node* actual_node = base_node;
-    for(int curr_index = initial_depth; curr_index < word.size(); curr_index++){
-        // if no next edge found, add the node
+    for(int curr_index = common_prefix_size; curr_index < word.size(); curr_index++){
         char current_char = word[curr_index];
         Transition* transition = new Transition(current_char);
         this->add_node(actual_node, transition);
-        actual_node = get_next_node_with_last_char(actual_node);
+        actual_node = get_last_next_node(actual_node);
     }
     actual_node->valid = true;
 }
 
-std::string FST::get_last_word_suffix(Node* node){
-    std::string suffix = "";
-    Node* actual_node = node;
-    while(!actual_node->next_nodes.empty()){
-        int forward_transitions_size = actual_node->forward_transitions.size();
-        suffix.push_back(actual_node->forward_transitions[forward_transitions_size - 1]->character);
-        actual_node = actual_node->next_nodes[forward_transitions_size - 1];
-    }
-    return suffix;
-}
-
+/// @brief Before add a new word, updates the fst, by joining the new word suffix with a previous frozen word suffix
+/// @param branch_node: the node with the biggest common prefix with the new word that will been added 
 void FST::update(Node* branch_node){
+    // Do nothing if there is no frozen node yet 
+    // Cases where the word been added is the first word, or the first word with a new suffix
     if(final_frozen_node == nullptr)
         return;
+    // Do nothing if the branch node have no next nodes 
+    // Cases where the word been added is a continuation of the last word added
     if(branch_node->next_nodes.empty())
         return;
-    // Get last node
+
+    // Get last node added previously
     Node* last_new_node = branch_node;
     while(!last_new_node->next_nodes.empty())
-        last_new_node = get_next_node_with_last_char(last_new_node);
+        last_new_node = get_last_next_node(last_new_node);
 
+    // Get an frozen node equals to a related no frozen node that can be replaced by the first one
     Node* actual_new_node = last_new_node;
     Node* actual_frozen_node = final_frozen_node;
     Node* next_node;
+    // In each iteration, compares if actual_new_node and actual_frozen_node are equal and if both are valid for a new transition
     while(actual_new_node != branch_node){
         next_node = nullptr;
         char curr_desired_char = actual_new_node->backward_transitions[actual_new_node->backward_transitions.size() - 1]->character;
         Node* next_new_node = actual_new_node->previous_nodes[actual_new_node->previous_nodes.size() - 1];
-        if(next_new_node == branch_node)
+        // The branch node is not part of the suffix that was added and the next node must not have branches
+        if(next_new_node == branch_node || next_new_node->next_nodes.size() > 1)
             break;
         for(int i = 0; i < actual_frozen_node->previous_nodes.size(); i++){
-            if(!actual_frozen_node->previous_nodes[i]->frozen)
+            Node* previous_node = actual_frozen_node->previous_nodes[i];
+            // The previous node must be frozen
+            if(!previous_node->frozen)
                 continue;
-            if(actual_frozen_node->previous_nodes[i]->valid != next_new_node->valid)
+            // The previous node must have the same valid state as the next_new_node
+            if(previous_node->valid != next_new_node->valid)
                 continue;
+            // The previous node must not have branches
+            if(previous_node->next_nodes.size() > 1)
+                continue;
+            
             if(actual_frozen_node->backward_transitions[i]->character == curr_desired_char){
-                next_node = actual_frozen_node->previous_nodes[i];
+                next_node = previous_node;
                 break;
             }
         }
+        // No equals node found
         if(next_node == nullptr)
-            break;
-        if(next_node->next_nodes.size() > 1 || next_new_node->next_nodes.size() > 1)
             break;
         actual_frozen_node = next_node;
         actual_new_node = next_new_node;
     }
+
+    // Updates the transitions
     actual_new_node = actual_new_node->previous_nodes[actual_new_node->previous_nodes.size() - 1];
     actual_new_node->next_nodes[actual_new_node->next_nodes.size() - 1] = actual_frozen_node;
     actual_frozen_node->previous_nodes.push_back(actual_new_node);
     actual_frozen_node->backward_transitions.push_back(actual_new_node->forward_transitions[actual_new_node->forward_transitions.size() - 1]);
 }
 
+/// @brief Check if the file contains sorted words
+/// @return if the file is sorted
 bool FST::check_data(const std::string& filename){
     std::ifstream myfile;
     myfile.open(filename);
--- fst.h
+++ fst.h
@@ -20,16 +20,15 @@ private:
     Node* final_frozen_node;
     // Build utils
     void froze_node_tree(Node* node);
-    void add_suffix(Node* base_node, const std::string& word, int initial_depth);
+    void add_suffix(Node* base_node, const std::string& word, int common_prefix_size);
     void add_node(Node* base_node, Transition* transition);
     void update(Node* branch_node);
-    static Node* get_next_node_with_last_char(Node* node);
-    static char get_next_last_char(Node* node);
-    Node* get_new_word_max_common_preffix(const std::string& new_word, int& common_prefix_size);
+    static Node* get_last_next_node(Node* node);
+    static char get_last_next_char(Node* node);
+    Node* get_new_word_max_existent_prefix(const std::string& new_word, int& existent_prefix_size);
     // Search utils
     void DFS(Node* base_node, const std::string& word, std::vector<std::string>& output_words, int max_num_of_results);
     Node* retrieve_node_with_prefix(const std::string& prefix);
-    std::string get_last_word_suffix(Node* node);
 
 };
 
