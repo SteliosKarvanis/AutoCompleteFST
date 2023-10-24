2bcc1ef26855269419d7055284c976141f6ee368
stelios <stelioscbranco@hotmail.com>
2023-10-24 20:01:35 -0300

    adding multiple functions to gui

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,12 +1,28 @@
 #include "gui.h"
 
+std::string MODE_TO_STRING(MODE MODE) {
+    switch (MODE) {
+        case MODE::AUTOCOMPLETE:
+            return "AUTOCOMPLETE";
+        case MODE::LEVENSHTEIN:
+            return "LEVENSHTEIN";
+        default:
+            return "NONE";
+    }
+}
+
 AutoCompleteUI::AutoCompleteUI(QWidget* parent, std::string data_file) : QMainWindow(parent){
     setWindowTitle("AutoComplete");
 
     this->central_widget = nullptr;
     this->display_label = nullptr;
     this->input_field = nullptr;
+    this->mode_box = nullptr;
+    this->levenstein_distance_label = nullptr;
+
     this->fst = new FST();
+    this->current_mode = MODE::AUTOCOMPLETE;
+    this->levenstein_distance = 0;
 
     this->fst->buildFST(data_file);
     this->create_widgets();
@@ -18,7 +34,7 @@ AutoCompleteUI::AutoCompleteUI(QWidget* parent, std::string data_file) : QMainWi
 
 void AutoCompleteUI::get_predictions(){
     std::string input_text = this->input_field->text().toStdString();
-    auto words = fst->retrieve_words(input_text);
+    auto words = this->result_factory(input_text);
     std::string output_text;
     for(auto word : words){
         output_text = output_text + word;
@@ -29,22 +45,63 @@ void AutoCompleteUI::get_predictions(){
     this->adjustSize();
 }
 
+void AutoCompleteUI::change_mode(int mode_idx){
+    this->current_mode = static_cast<MODE>(mode_idx);
+    if(this->current_mode == MODE::LEVENSHTEIN)
+        this->levenstein_distance_label->setVisible(true);
+    else
+        this->levenstein_distance_label->setVisible(false);
+    this->update();
+}
+
+void AutoCompleteUI::change_levenstein_distance(){
+    if(this->levenstein_distance_label->hasAcceptableInput() == false)
+        return;
+
+    std::string input_text = this->levenstein_distance_label->text().toStdString();
+    this->levenstein_distance = std::stoi(input_text);
+}
+
+std::vector<std::string> AutoCompleteUI::result_factory(const std::string& word){
+    if(this->current_mode == MODE::AUTOCOMPLETE)
+        return this->fst->retrieve_words(word);
+    else
+        return this->fst->levestein(word, this->levenstein_distance);
+}
+
 void AutoCompleteUI::create_actions(){
     connect(input_field, &QLineEdit::textChanged, this, &AutoCompleteUI::get_predictions);
+    connect(mode_box, SIGNAL(currentIndexChanged(int)), this, SLOT(change_mode(int)));
+    connect(levenstein_distance_label, &QLineEdit::textChanged, this, &AutoCompleteUI::change_levenstein_distance);
 }
 
 void AutoCompleteUI::create_widgets(){
     this->central_widget = new QWidget(this);
     this->display_label = new QLabel(central_widget);
+    this->input_field = new QLineEdit(central_widget);
+    
     this->display_label->setAlignment(Qt::AlignTop);
     this->display_label->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
     this->display_label->setWordWrap(true);
-    this->input_field = new QLineEdit(central_widget);
+    
+    this->mode_box = new QComboBox(central_widget);
+    for(int i = 0; i < static_cast<int>(MODE::MODE_COUNT); i++){
+        this->mode_box->addItem(QString::fromStdString(MODE_TO_STRING(static_cast<MODE>(i))));
+    }
 
+    this->levenstein_distance_label = new QLineEdit(central_widget);
+    this->levenstein_distance_label->setText(QString::fromStdString("0"));
+    this->levenstein_distance_label->setVisible(false);
+    this->levenstein_distance_label->setValidator(new QIntValidator(0, INT_MAX));
 }
 
 void AutoCompleteUI::create_layout(){
+    QHBoxLayout* mode_layout = new QHBoxLayout();
+    mode_layout->addWidget(mode_box);
+    mode_layout->addWidget(levenstein_distance_label);
+
     QVBoxLayout* layout = new QVBoxLayout();
+    layout->addLayout(mode_layout);
     layout->addWidget(input_field);
     layout->addWidget(display_label);
     this->central_widget->setLayout(layout);
--- src/gui.h
+++ src/gui.h
@@ -6,10 +6,25 @@
 #include <QLineEdit>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QComboBox>
+#include <QIntValidator>
+#include <unordered_map>
 
 #include <iostream>
 #include "fst.h"
 
+enum class MODE {
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    /////////// Update MODE_TO_STRING, when changing this enum ////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    AUTOCOMPLETE = 0,
+    LEVENSHTEIN = 1,
+    MODE_COUNT = 2,
+};
+
+std::string MODE_TO_STRING(MODE MODE);
 
 class AutoCompleteUI : public QMainWindow {
 Q_OBJECT
@@ -20,12 +35,20 @@ public:
 
 public slots:
     void get_predictions();
+    void change_mode(int mode_idx);
+    void change_levenstein_distance();
 
 private:
     FST* fst;
     QWidget* central_widget;
     QLineEdit* input_field;
     QLabel* display_label;
+    QComboBox* mode_box;
+    QLineEdit* levenstein_distance_label;
+
+    MODE current_mode;
+    int levenstein_distance;
+    std::vector<std::string> result_factory(const std::string& word);
 
     void create_actions();
     void create_layout();
