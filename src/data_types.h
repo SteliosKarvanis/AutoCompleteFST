2fa95706240ef806eb878ef07c36241d9f536929
stelios <stelioscbranco@hotmail.com>
2023-11-21 16:42:03 -0300

    remobing unnecessary functions

--- src/data_types.h
+++ src/data_types.h
@@ -6,13 +6,6 @@
 struct Node{
     bool valid = false;
     std::map<char, Node*> next_nodes = std::map<char, Node*>();
-    bool equals(Node* other_node){
-        if((this->valid != other_node->valid))
-            return false;
-        if(this->next_nodes != other_node->next_nodes)
-            return false;
-        return true;
-    }
 };
 
 #endif
